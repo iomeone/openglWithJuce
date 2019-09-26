53c941c861aacf27d664c2ece17d9cb06acd3a3a
iomeone <iomeonee@gmail.com>
2019-09-26 16:41:51 +0800

    tutorial one is done

--- Source/ShaderProgram.h
+++ Source/ShaderProgram.h
@@ -12,198 +12,6 @@
 #include "../JuceLibraryCode/JuceHeader.h"
 
 
-
-
-#pragma pack(1)
-
-
-struct Position {
-	float x;
-	float y;
-};
-
-struct ColorRGBA8 {
-	ColorRGBA8() : r(0), g(0), b(0), a(0) { }
-	ColorRGBA8(GLubyte R, GLubyte G, GLubyte B, GLubyte A) :
-		r(R), g(G), b(B), a(A) { }
-	GLubyte r;
-	GLubyte g;
-	GLubyte b;
-	GLubyte a;
-};
-
-struct UV {
-	float u;
-	float v;
-};
-
-//The vertex definition
-struct Vertex {
-
-	Position position;
-
-	//4 bytes for r g b a color.
-	ColorRGBA8 color;
-
-	//UV texture coordinates.
-	UV uv;
-
-	void setPosition(float x, float y) {
-		position.x = x;
-		position.y = y;
-	}
-
-	void setColor(GLubyte r, GLubyte g, GLubyte b, GLubyte a) {
-		color.r = r;
-		color.g = g;
-		color.b = b;
-		color.a = a;
-	}
-
-	void setUV(float u, float v) {
-		uv.u = u;
-		uv.v = v;
-	}
-};
-#pragma pack()
-
-struct Attributes
-{
-	Attributes(OpenGLContext& openGLContext, OpenGLShaderProgram& shader)
-	{
-		position = createAttribute(openGLContext, shader, "aPos");
-		/*normal = createAttribute(openGLContext, shader, "normal");
-		sourceColour = createAttribute(openGLContext, shader, "sourceColour");*/
-		//textureCoordIn = createAttribute(openGLContext, shader, "textureCoordIn");
-	}
-
-	void enable(OpenGLContext& openGLContext)
-	{
-		if (position != nullptr)
-		{
-			openGLContext.extensions.glEnableVertexAttribArray(position->attributeID);
-			openGLContext.extensions.glVertexAttribPointer(position->attributeID, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), 0);
-
-		}
-
-		if (textureCoordIn != nullptr)
-		{
-			openGLContext.extensions.glVertexAttribPointer(textureCoordIn->attributeID, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)(sizeof(float) * 2));
-			openGLContext.extensions.glEnableVertexAttribArray(textureCoordIn->attributeID);
-		}
-	}
-
-	void disable(OpenGLContext& openGLContext)
-	{
-		if (position != nullptr)       openGLContext.extensions.glDisableVertexAttribArray(position->attributeID);
-		/*	if (normal != nullptr)         openGLContext.extensions.glDisableVertexAttribArray(normal->attributeID);
-		if (sourceColour != nullptr)   openGLContext.extensions.glDisableVertexAttribArray(sourceColour->attributeID);*/
-		if (textureCoordIn != nullptr)  openGLContext.extensions.glDisableVertexAttribArray(textureCoordIn->attributeID);
-	}
-
-	bool getStatus()
-	{
-		return true;
-		if (position == nullptr)
-			return false;
-		else
-			return true;
-	}
-	ScopedPointer<OpenGLShaderProgram::Attribute> position, textureCoordIn;
-
-private:
-	static OpenGLShaderProgram::Attribute* createAttribute(OpenGLContext& openGLContext,
-		OpenGLShaderProgram& shader,
-		const char* attributeName)
-	{
-		if (openGLContext.extensions.glGetAttribLocation(shader.getProgramID(), attributeName) < 0)
-			return nullptr;
-
-		return new juce::OpenGLShaderProgram::Attribute(shader, attributeName);
-	}
-};
-
-
-
-struct Uniforms
-{
-	Uniforms(OpenGLContext& openGLContext, OpenGLShaderProgram& shader)
-	{
-		projectionMatrix = createUniform(openGLContext, shader, "projectionMatrix");
-		viewMatrix = createUniform(openGLContext, shader, "viewMatrix");
-		ProjectionView = createUniform(openGLContext, shader, "ProjectionView");
-		texture = createUniform(openGLContext, shader, "Texture_1");
-		texture2 = createUniform(openGLContext, shader, "Texture_2");
-		texture3 = createUniform(openGLContext, shader, "Texture_3");
-		texture4 = createUniform(openGLContext, shader, "Texture_4");
-
-		lightPosition = createUniform(openGLContext, shader, "lightPosition");
-		iGlobalTime = createUniform(openGLContext, shader, "iGlobalTime");
-
-		x = createUniform(openGLContext, shader, "_x");
-		y = createUniform(openGLContext, shader, "_y");
-		w = createUniform(openGLContext, shader, "_w");
-		h = createUniform(openGLContext, shader, "_h");
-
-		iMouse = createUniform(openGLContext, shader, "iMouse");
-		iResolution = createUniform(openGLContext, shader, "iResolution");
-
-
-		winW = createUniform(openGLContext, shader, "winW");
-		winH = createUniform(openGLContext, shader, "winH");
-
-		arrFloat = createUniform(openGLContext, shader, "arrFloat");
-
-	}
-	bool getStatus()
-	{
-		return true;
-
-	}
-	ScopedPointer<OpenGLShaderProgram::Uniform>projectionMatrix, viewMatrix, ProjectionView, lightPosition, iGlobalTime, arrFloat, texture, texture2, texture3, texture4;
-	ScopedPointer<OpenGLShaderProgram::Uniform> x, y, w, h, winW, winH, iMouse, iResolution;
-
-private:
-	static OpenGLShaderProgram::Uniform* createUniform(OpenGLContext& openGLContext,
-		OpenGLShaderProgram& shader,
-		const char* uniformName)
-	{
-		if (openGLContext.extensions.glGetUniformLocation(shader.getProgramID(), uniformName) < 0)
-			return nullptr;
-
-		return new OpenGLShaderProgram::Uniform(shader, uniformName);
-	}
-};
-
-
-
-struct DynamicTexture
-{
-	DynamicTexture() {  }
-
-	Image image;
-
-
-	bool applyTo(OpenGLTexture& texture, juce::String p)
-	{
-		const int size = 16 * 16;
-
-		//if (!image.isValid())
-		{
-			File f = p;
-			if (!f.existsAsFile())
-			{
-				jassertfalse;
-				//msg("file " + p + " is not exist!");
-			}
-			else
-				image = juce::ImageFileFormat::loadFrom(f);
-		}
-		texture.loadImage(image);
-		return true;
-	}
-};
-
 class ShaderProgram :public Thread
 {
 public:
@@ -326,7 +134,7 @@ public:
 		}
 		if (_strFragment.length() == 0 || _strVertex.length() == 0)
 			return false;
-		auto _compileResult = getCompileResult();
+		
 		ScopedPointer<OpenGLShaderProgram> newShader(new OpenGLShaderProgram(_openGLContext));
 		jassert(_openGLContext.isActive());
 		jassert(_openGLContext.isAttached());
@@ -336,49 +144,27 @@ public:
 		{
 			_shader = nullptr;
 			_shader = newShader;
-
-			_attributes = nullptr;
-			_attributes = new Attributes(_openGLContext, *_shader);
-
-			_uniforms = nullptr;
-			_uniforms = new Uniforms(_openGLContext, *_shader);
-
-	
 			_compileResult = "GLSL: v" + String(juce::OpenGLShaderProgram::getLanguageVersion(), 2);
-			
-				//const MessageManagerLock mmLock;
-				//if (mmLock.lockWasGained())
-				//{
-				//	AlertWindow::showMessageBox(AlertWindow::AlertIconType::InfoIcon, "GLSL Success", _compileResult);
-				//}
 			
 		}
 		else
 		{
 			_compileResult = newShader->getLastError();
-		
 
-
-				//_compileResult = s;
-
-				//const MessageManagerLock mmLock;
-
-				//if (mmLock.lockWasGained())
-				//	;
-			
 		}
 		_strVertex = String();
 		_strFragment = String();
 		return true;
 	}
 
-	ScopedPointer<OpenGLShaderProgram> _shader{ nullptr };
-
-	static String& getCompileResult()
+	String getCompileResult()
 	{
-		static String _compileResult;
 		return _compileResult;
 	}
+
+	ScopedPointer<OpenGLShaderProgram> _shader{ nullptr };
+
+ 
 
 private:
 	
@@ -391,14 +177,13 @@ private:
 	juce::Time lastModiyFragment;
 	juce::Time lastModiyVertex;
 
-	ScopedPointer<Attributes> _attributes;
-	ScopedPointer<Uniforms> _uniforms;
-
  
 
 	OpenGLContext & _openGLContext;
 
 	juce::Atomic<bool> _isReadingFile{ false };
+
+	String _compileResult;
 
 	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(ShaderProgram)
 };
