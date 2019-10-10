ef3f8d89aa788c8748b9b752cdc8b20b5fbaa41d
iomeone <iomeonee@gmail.com>
2019-10-10 17:48:42 +0800

    disable GL_BLEND  model show right, but texture cordinate seems wrong?

--- Source/Tutorial15.h
+++ Source/Tutorial15.h
@@ -93,6 +93,7 @@ namespace T15 {
 					_lblCompileInfo->setText(_shaderProgram->getCompileResult() + " \n" + tutorialLink, NotificationType::dontSendNotification);
 				}
 			}
+			glDisable(GL_BLEND);
 			glEnable(GL_DEPTH_TEST);
 			OpenGLHelpers::clear(juce::Colour(0.2f, 0.3f, 0.3f, 1.0f));
 			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
