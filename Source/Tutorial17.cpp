e2230037c02d113d04dbecdb95d7b44121d65514
iomeone <iomeonee@gmail.com>
2019-10-12 15:45:07 +0800

    fix: fix blend bug

--- Source/Tutorial17.cpp
+++ Source/Tutorial17.cpp
@@ -445,7 +445,8 @@ namespace T17 {
 
 
 			glEnable(GL_DEPTH_TEST);
-			glDepthFunc(GL_LESS);
+			glEnable(GL_BLEND);
+			glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 		
 			if (_shaderProgramScene->_shader )
 			{
